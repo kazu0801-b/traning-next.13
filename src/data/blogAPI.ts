c37ff74a999f90c62b0e2188bb19668dd52722c1
kazu0801-b <chenglingmu584@gmail.com>
2025-06-02 22:04:03 +0900

    エラーが発生しない

--- src/data/blogAPI.ts
+++ src/data/blogAPI.ts
@@ -1,15 +1,22 @@
 import { Article } from "./types";
 
-export const getAllArticles = async () :Promise<Article[]>=> {
-    try {
-        const res = await fetch('http://localhost:3001/posts', { cache: "no-store" }); // SSR
-        if (!res.ok) {
-            throw new Error(`Failed to fetch articles: ${res.statusText}`);
-        }
-        const articles = await res.json();
-        return articles;
-    } catch (error) {
-        console.error(error);
-        return [];
+export const getAllArticles = async (): Promise<Article[]> => {
+  try {
+    const res = await fetch("http://localhost:3001/posts", {
+      cache: "no-store",
+    }); // SSR
+    if (!res.ok) {
+      throw new Error(`Failed to fetch articles: ${res.statusText}`);
     }
+
+    throw new Error("エラーが発生しました。");
+
+    await new Promise((resolve) => setTimeout(resolve, 1500)); // Simulate delay
+
+    const articles = await res.json();
+    return articles;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
