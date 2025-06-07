6b7ceed56a3657dbd30943cd756f8c27d8d95eb8
kazu0801-b <chenglingmu584@gmail.com>
2025-06-07 17:26:51 +0900

    chapter28途中

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
-const Article = () => {
-  return <div>page</div>;
+const Article = ({ params }: { params: { id: string } }) => {
+  return (
+    <div className="max-w-3xl mx-auto p-5">
+      page
+      <Image src="https://picsum.photos/800" alt="" width={1280} height={300} />
+      <h1 className="text-4xl text-center mb-10 mt-10">ここがタイトルです</h1>
+      <div className="text-lg leading-relaxed text-justify">
+        <p>ここが本文です</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/data/blogAPI.ts
+++ src/data/blogAPI.ts
@@ -1,22 +1,34 @@
+// export const getAllArticles = async (): Promise<Article[]> => {
+//   try {
+//     const res = await fetch("http://localhost:3001/posts", {
+//       cache: "no-store",
+// }); // SSR
+// if (!res.ok) {
+// throw new Error(`Failed to fetch articles: ${res.statusText}`);
+// }
+
+//     await new Promise((resolve) => setTimeout(resolve, 1500)); // Simulate delay
+
+//     const articles = await res.json();
+//     return articles;
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// };
+
+// "use client";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
-  try {
-    const res = await fetch("http://localhost:3001/posts", {
-      cache: "no-store",
-    }); // SSR
-    if (!res.ok) {
-      throw new Error(`Failed to fetch articles: ${res.statusText}`);
-    }
+  const res = await fetch("http://localhost:3001/posts", { cache: "no-store" });
 
-    throw new Error("エラーが発生しました。");
-
-    await new Promise((resolve) => setTimeout(resolve, 1500)); // Simulate delay
-
-    const articles = await res.json();
-    return articles;
-  } catch (error) {
-    console.error(error);
-    return [];
+  if (!res.ok) {
+    throw new Error("エラーが発生しました");
   }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500)); // Simulate delay
+
+  const articles = await res.json();
+  return articles;
 };
